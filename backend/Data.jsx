f4d91c5fb68abbe557b1dabb1875c9216c9b83d7
son goku <144996229+son-goku-2008@users.noreply.github.com>
2024-12-20 14:51:08 +0530

    backend

---
+++ backend/Data.jsx
@@ -0,0 +1,44 @@
+export const Offers = [
+  {
+    Name: "🔥Popular",
+  },
+  {
+    Name: "🔥Tranding",
+  },
+  {
+    Name: "🔥Toys",
+  },
+  {
+    Name: "🔥Dj",
+  },
+  {
+    Name: "🔥Sharts",
+  },
+  {
+    Name: "🔥Bike",
+  },
+  {
+    Name: "🔥Adani",
+  },
+  {
+    Name: "🔥Tranding",
+  },
+  {
+    Name: "🔥Electornics",
+  },
+  {
+    Name: "🔥Mobile",
+  },
+  {
+    Name: "🔥Shoes",
+  },
+  {
+    Name: "🔥Headphones",
+  },
+  {
+    Name: "🔥hats",
+  },
+  {
+    Name: "🔥Fashon",
+  },
+];
